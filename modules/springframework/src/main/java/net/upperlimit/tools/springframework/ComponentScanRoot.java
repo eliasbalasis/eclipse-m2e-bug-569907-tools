85884c1c150c390ec9a9776579a07f08009766ad
bail8001 <ilias.balasis@nielseniq.com>
2021-01-05 21:03:34 +0000

    first commit

---
+++ modules/springframework/src/main/java/net/upperlimit/tools/springframework/ComponentScanRoot.java
@@ -0,0 +1,9 @@
+package net.upperlimit.tools.springframework;
+
+/**
+ * Indicator for SpringFramework component scan root
+ * 
+ * @author bail8001
+ */
+public interface ComponentScanRoot {
+}
---
+++ modules/springframework/src/main/java/net/upperlimit/tools/springframework/context/SpringFrameworkContextAccessComponentScanRoot.java
@@ -0,0 +1,6 @@
+package net.upperlimit.tools.springframework.context;
+
+import net.upperlimit.tools.springframework.ComponentScanRoot;
+
+public final class SpringFrameworkContextAccessComponentScanRoot implements ComponentScanRoot {
+}
